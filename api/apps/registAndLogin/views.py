9fa5c19d7e3b11529cd1db97cba64bc018722045
OcaenEyes <1450136519@qq.com>
2019-07-20 16:22:06 +0800

    master

--- api/apps/registAndLogin/views.py
+++ api/apps/registAndLogin/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from .models import UserInfo
+from .form import *
 
 # Create your views here.
+
+def index(request):
+    user = request.session.get('user',False)
+
+    return render(request,'index.tml')
+
+
+def registAndLogin(request):
+    user = request.session.get('user',False)
+    print(user)
+    if not user:
+        return render(request,'login.html')
+    else:
+        return HttpResponseRedirect('/index/')
+
+
+def regist(request):
+    check =False
+    if request.method == 'POST':
+        form =
